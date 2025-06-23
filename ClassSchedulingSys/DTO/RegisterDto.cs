09bfdcd76b6db6f9e7c1b61f5c613ffe628239bd
kennethtablang <kennethreytablang@gmail.com>
2025-06-23 09:08:13 +0800

    Adding additional properties for the Registration

--- ClassSchedulingSys/DTO/RegisterDto.cs
+++ ClassSchedulingSys/DTO/RegisterDto.cs
@@ -2,6 +2,10 @@
 {
     public class RegisterDto
     {
+        public string FirstName { get; set; } = null!;
+        public string? MiddleName { get; set; } // Optional
+        public string LastName { get; set; } = null!;
+
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
         public string ConfirmPassword { get; set; } = null!;
